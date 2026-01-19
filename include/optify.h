45be3eb01d4b69ab08a8463f02649e4e880f0e55
Jason Pena <jasonpena@awkless.com>
2026-01-18 23:21:30 -0700

    chore: initial CMake setup
    Basic setup of the build system and codebase. Created the
    `optify::optify` target for the user to utilize if they decide to use
    the project with CMake.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ include/optify.h
@@ -0,0 +1,10 @@
+/* SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef OPTIFY_H
+#define OPTIFY_H
+
+#include "optify_export.h"
+
+#endif /* OPTIFY_H */
---
+++ src/optify.c
@@ -0,0 +1,5 @@
+/* SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "optify.h"
