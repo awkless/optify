d69167d7fd6061c54770ddee012e2ca6f8f2817d
Jason Pena <jasonpena@awkless.com>
2026-02-09 12:57:30 -0700

    chore: initial CMake setup
    Initial development environment setup through CMake. I added the
    `WARNINGS_AS_ERRORS` option to control the treatment of warnings during
    compilation. I also added the `CLANG_TIDY_CHECK` option to control
    static analysis through clang-tidy.
    
    This currently development environment allows the maintainer to either
    install a static or dynamic version of the library. It also allows the
    maintainer to integrate the library into other CMake projects through
    the use of the `find_package` command.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ include/optify.h
@@ -0,0 +1,14 @@
+/* SPDX-FileCopyrightText: 2026 Jason Pena <jasonpena@awkless.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef OPTIFY_H
+#define OPTIFY_H
+
+#include "optify_export.h"
+
+/* TODO: Write the library! */
+void
+stub(void);
+
+#endif /* OPTIFY_H */
---
+++ src/optify.c
@@ -0,0 +1,10 @@
+/* SPDX-FileCopyrightText: 2026 Jason Pena <jasonpena@awkless.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+/* TODO: Write the library! */
+void
+stub(void)
+{
+	return;
+}
