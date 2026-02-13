c78b3fa4faea5702d04c8c3263243fbcce7d4c33
Jason Pena <jasonpena@awkless.com>
2026-02-13 08:21:35 -0700

    feat: implement short option parsing
    Now the `optify_parse` function can properly parse short options defined
    through an option entry list. I added some test cases in the new
    `tests/test_optify_parse.c` file to ensure that the behavior of short
    option parsing actually works.
    
    I have not implemented short option argument parsing just yet. Right now
    I am just focused on getting the parser to detect and parse short
    options. I also want the parser to detect unknown short options not
    defined in the option entry list.
    
    Finally, positionals are not handled, but simply ignored by the parser
    for the moment. Eventually, the logic to actually handle positionals
    properly will be added at some point.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/optify.c
+++ src/optify.c
@@ -2,6 +2,8 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <string.h>
+
 #include "optify.h"
 
 int
@@ -14,17 +16,81 @@ optify_init(struct optify *self, char **argv, int argc)
 	self->optarg = NULL;
 	self->errarg = NULL;
 	self->argc = argc;
+	/* INVARIANT: Start at argv[1], because argv[0] is the program name. */
 	self->optidx = 1;
 
 	return OPTIFY_SUCCESS;
 }
 
-int
-optify_parse(struct optify *self, struct optify_option *optlist, size_t optcnt)
+static inline int
+is_short_option(const char *arg)
 {
-	(void)self;
-	(void)optlist;
-	(void)optcnt;
+	return arg != NULL && arg[0] == '-' && arg[1] != '-' && arg[1] != '\0';
+}
 
-	return OPTIFY_PARSE_END;
+static inline int
+is_option_entry_empty(const struct optify_option *entry)
+{
+	return entry->shortid != 0 && entry->longid != NULL &&
+	       entry->argkind != 0;
+}
+
+static const struct optify_option *
+get_short_option_entry(const char *arg, const struct optify_option *optlist,
+	               size_t optcnt)
+{
+	const struct optify_option *opt = NULL;
+	const struct optify_option *end = optlist + optcnt;
+
+	if (!arg || !optlist)
+		return NULL;
+
+	/*
+	 * SAFETY: Remain in bounds of option entry list.
+	 *   - Length check takes precedence over NULL termination check in
+	 *     case caller submits an option list with no NULL terminator.
+	 */
+	for (opt = optlist; opt < end && !is_option_entry_empty(opt); ++opt)
+		if (arg[1] == opt->shortid)
+			return opt;
+
+	return NULL;
+}
+
+int
+optify_parse(struct optify *self, const struct optify_option *optlist, size_t optcnt)
+{
+	const struct optify_option *opt = NULL;
+	char *arg = NULL;
+
+	if (!self || !optlist)
+		return -OPTIFY_BAD_API_USE;
+
+	self->optarg = NULL;
+	self->errarg = NULL;
+	arg = self->argv[self->optidx];
+
+	/*
+	 * INVARIANT: Stop parsing iff we reach the end of argv.
+	 *   - C standard ensures argv is NULL terminated so length check is
+	 *     technically redundant, but I am very paranoid :3
+	 */
+	if (!arg || self->optidx == self->argc)
+		return OPTIFY_PARSE_END;
+
+	if (is_short_option(arg)) {
+		opt = get_short_option_entry(arg, optlist, optcnt);
+		if (!opt) {
+			self->errarg = arg;
+			/* INVARIANT: Move forward; let caller handle this. */
+			++self->optidx;
+			return -OPTIFY_UNKNOWN_OPT;
+		}
+
+		++self->optidx;
+		return opt->shortid;
+	}
+
+	++self->optidx;
+	return OPTIFY_PARSE_CONTINUE;
 }
