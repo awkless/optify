e1a8b97443a16e5b6a5e740c2653d85308e89241
Jason Pena <jasonpena@awkless.com>
2026-02-19 16:54:15 -0700

    refactor: improve naming and docs in current test cases
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- tests/test_optify_init.c
+++ tests/test_optify_init.c
@@ -6,29 +6,36 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <setjmp.h>
 #include <cmocka.h>
 
 #include <optify.h>
 
+/*
+ * Test that `optify_init` catches invalid parameters.
+ *
+ * Make sure that `optify_init` catches the following:
+ *   - Parameter `self` is NULL.
+ *   - Parameter `argv` is NULL.
+ */
 static void
-catch_null_parser_state(void **state)
+catch_invalid_parameters(void **state)
 {
 	(void)state;
-	struct optify pstate = {0};
+	struct optify argp = { 0 };
 	char *argv[] = { "hello", NULL };
+	int argc = 1;
 	int result = 0;
 
 	result = optify_init(NULL, NULL, 0);
 	assert_int_equal(result, -OPTIFY_ERR_INVALID_PARAM);
 
-	result = optify_init(&pstate, NULL, 0);
+	result = optify_init(&argp, NULL, 0);
 	assert_int_equal(result, -OPTIFY_ERR_INVALID_PARAM);
 
-	result = optify_init(NULL, argv, 1);
+	result = optify_init(NULL, argv, argc);
 	assert_int_equal(result, -OPTIFY_ERR_INVALID_PARAM);
 
-	result = optify_init(&pstate, argv, 1);
+	result = optify_init(&argp, argv, argc);
 	assert_int_equal(result, OPTIFY_OK);
 }
 
@@ -37,7 +44,7 @@ main(void)
 {
 	int result = 0;
 	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(catch_null_parser_state)
+		cmocka_unit_test(catch_invalid_parameters)
 	};
 
 	result = cmocka_run_group_tests(tests, NULL, NULL);
